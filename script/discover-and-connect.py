31e207aa7540cbea9c33599392f3f85331886342
Sami Alaoui <4ndroidgeek@gmail.com>
2023-05-07 20:51:14 +0100

    Improve mDNS discovery script

--- script/discover-and-connect.py
+++ script/discover-and-connect.py
@@ -1,16 +1,33 @@
+#!/usr/bin/env python3
+
 from os import _exit
-from subprocess import Popen
+from subprocess import Popen, PIPE
 from zeroconf import ServiceBrowser, ServiceListener, Zeroconf
 
 class MyListener(ServiceListener):
-    def add_service(self, zc: Zeroconf, type_: str, name: str) -> None:
+
+    def do_stuff(self, zc: Zeroconf, type_: str, name: str) -> None:
         info = zc.get_service_info(type_, name)
         ip_bytes = info.addresses[0]
         ip_str = f"{ip_bytes[0]}.{ip_bytes[1]}.{ip_bytes[2]}.{ip_bytes[3]}"
-        print(f"\nFound: {ip_str}:{info.port}")
-        Popen(['adb', 'connect', f"{ip_str}:{info.port}"]).wait()
-        zeroconf.close()
-        _exit(0)
+        print(f"Found: {ip_str}:{info.port}")
+        pipe = Popen(['adb', 'connect', f"{ip_str}:{info.port}"], stdout=PIPE)
+        output = pipe.communicate()[0].decode("utf-8")
+        pipe.wait()
+        if output.startswith("connected"):
+            print(output)
+            zeroconf.close()
+            _exit(0)
+
+    def add_service(self, zc: Zeroconf, type_: str, name: str) -> None:
+        self.do_stuff(zc, type_, name)
+    
+    def update_service(self, zc: Zeroconf, type_: str, name: str) -> None:
+        self.do_stuff(zc, type_, name)
+ 
+    def remove_service(self, zc: Zeroconf, type_: str, name: str) -> None:
+        return
+
 
 zeroconf = Zeroconf()
 listener = MyListener()
